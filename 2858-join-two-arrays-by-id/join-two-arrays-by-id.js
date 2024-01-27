cd2d0a976a564a751d7223ca1a2a78dc3cd7a087
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-01-28 00:04:00 +0530

    Time: 264 ms (94.14%) | Memory: 91.1 MB (88.10%) - LeetSync

--- 2858-join-two-arrays-by-id/join-two-arrays-by-id.js
+++ 2858-join-two-arrays-by-id/join-two-arrays-by-id.js
@@ -8,10 +8,13 @@ var join = function(arr1, arr2) {
     for (let i = 0; i < arr1.length; i++) {
         result[arr1[i].id] = arr1[i];
     } 
+
     for (let i = 0; i < arr2.length; i++) {
         if (result[arr2[i].id]) {
             for (const key in arr2[i]) result[arr2[i].id][key] = arr2[i][key];  
-        } else {
+        } 
+        
+        else {
             result[arr2[i].id] = arr2[i];
         }
     } 
