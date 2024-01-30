825d18b5a70a5a35d67fb51e41b3fdf66ee6ecb2
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-01-30 22:05:19 +0530

    Time: 1 ms (99.31%) | Memory: 43.5 MB (43.16%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -34,27 +34,29 @@ class Solution {
         Queue<Pair> queue = new LinkedList<>();
         queue.add(new Pair(root, 0));
 
+        // Level-Order Traversal
         while (!queue.isEmpty()) {
             int size = queue.size();
-            int min = queue.peek().index;
+            int min = queue.peek().index; // Keep track of min
 
             int first = 0;
             int last = 0;
 
             for (int i = 0; i < size; i++) {
-                int currentIndex = queue.peek().index - min;
+                int currentIndex = queue.peek().index - min; // Adjusting index based on the min index of the level
 
                 TreeNode node = queue.peek().node;
                 queue.poll();
 
                 if (i == 0) {
-                    first = currentIndex;
+                    first = currentIndex; // Recording the index of the leftmost TreeNode 
                 }
 
                 if (i == size - 1) {
-                    last = currentIndex;
+                    last = currentIndex; // Recording the index of the rightmost TreeNode 
                 }
 
+                // Add the left and right children to the queue with adjusted indices
                 if (node.left != null) {
                     queue.add(new Pair(node.left, currentIndex * 2 + 1));
                 }
@@ -64,7 +66,7 @@ class Solution {
                 }
             }
 
-            ans = Math.max(ans, last - first + 1);
+            ans = Math.max(ans, last - first + 1); // Updating max width
         }
 
         return ans;
