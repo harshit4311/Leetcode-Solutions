543b2b5d65ecd71b90198093236bed923792a6cf
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-02-14 19:35:55 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (11.68%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev, first, mid, last;
+
+    public void recoverTree(TreeNode root) {
+        first = mid = last = null;
+        prev = new TreeNode(Integer.MIN_VALUE);
+
+        inorder(root);
+
+        if(first != null && last != null){
+            int temp = first.val;
+            first.val = last.val;
+            last.val = temp;
+        }
+
+        else if(first != null && mid != null){
+            int temp = first.val;
+            first.val = mid.val;
+            mid.val = temp;
+        }
+    }
+
+    public void inorder(TreeNode root){
+        if(root == null) return;
+
+        inorder(root.left);
+
+        if(prev != null && (root.val < prev.val)){
+
+            //if this is the 1st violation, marks these nodes as 'first' and 'mid'
+            if(first == null){
+                first = prev;
+                mid = root;
+            }
+
+            // if this is the 2nd violation, mark this node as 'last'
+            else{
+                last = root;
+            }
+        }
+
+        // mark this node as prev
+        prev = root;
+        inorder(root.right);
+    }
+}
