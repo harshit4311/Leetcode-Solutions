46d3f0f3c6f4edc048300b0f483bcbef5e21fb64
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-02-16 16:39:01 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (75.37%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -14,50 +14,43 @@
  * }
  */
 class Solution {
-    TreeNode prev, first, mid, last;
+    public TreeNode prev = null, ans1 = null, ans2 = null;
 
-    public void recoverTree(TreeNode root) {
-        first = mid = last = null;
-        prev = new TreeNode(Integer.MIN_VALUE);
+    public void recoverTree(TreeNode root){    
+        if(root == null) return;
 
+        // Perform in-order traversal to find the swapped nodes
         inorder(root);
 
-        // swap the 'first' and 'last' nodes
-        if(first != null && last != null){
-            int temp = first.val;
-            first.val = last.val;
-            last.val = temp;
-        }
-
-        // swap the 'first' and 'mid' nodes
-        else if(first != null && mid != null){
-            int temp = first.val;
-            first.val = mid.val;
-            mid.val = temp;
-        }
+        // Swap the values
+        int temp = ans1.val;
+        ans1.val = ans2.val;
+        ans2.val = temp;
     }
 
     public void inorder(TreeNode root){
         if(root == null) return;
 
+        // Recursively traverse the left subtree
         inorder(root.left);
 
-        if(prev != null && (root.val < prev.val)){
+        // Check if the current node violates the BST property
+        if(prev != null){
 
-            // if this is the 1st violation, mark these nodes as 'first' and 'mid'
-            if(first == null){
-                first = prev;
-                mid = root;
-            }
+            // If this is the first violation, update the values of 'ans1' and 'ans2'
+            if(root.val < prev.val){
+                if(ans1 == null){
+                    ans1 = prev;
+                    ans2 = root;
+                }
 
-            // if this is the 2nd violation, mark this node as 'last'
-            else{
-                last = root;
+                // If this is the second violation, update the value of just 'ans2'
+                else{
+                    ans2 = root;
+                }
             }
         }
-
-        // mark this node as prev
         prev = root;
-        inorder(root.right);
+        inorder(root.right); // Recursively traverse the right subtree
     }
 }
