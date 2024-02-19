3d2f00373c4207195088bd19d61c98b38ac80c87
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-02-19 09:53:19 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (77.95%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,13 +14,26 @@
  * }
  */
 class Solution {
-    public int countNodes(TreeNode root){
-        if(root == null) return 0;
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
 
-        int leftCount = countNodes(root.left);
-        int rightCount = countNodes(root.right);
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
 
-        return leftCount + rightCount + 1;
-        
+        if(leftHeight == rightHeight){
+            // Perfectly balanced tree
+            return (1 << leftHeight) + countNodes(root.right);
+        } 
+        else{
+            // Not perfectly balanced, so recurse on left and right subtrees
+            return countNodes(root.left) + (1 << rightHeight);
+        }
+    }   
+
+    public int getHeight(TreeNode node) {
+        if(node == null) return 0;
+
+        // Assuming the tree is balanced, we only need to calculate the height of the left subtree
+        return 1 + getHeight(node.left); 
     }
 }
