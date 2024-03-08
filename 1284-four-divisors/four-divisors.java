3c637d0b39bc7362771a96a2ddae0fb07fc61e72
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-03-08 14:58:50 +0530

    Time: 36 ms (61.54%) | Memory: 44.4 MB (91.21%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -1,27 +1,28 @@
-class Solution {
+public class Solution {
+
     public int sumFourDivisors(int[] arr) {
         int result = 0;
 
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             int num = arr[i];
-            int divisorCount = countDivisors(arr[i]);
-            
-            if(divisorCount == 4){
+            int divisorCount = countDivisors(num);
+
+            if (divisorCount == 4) {
                 result += sumOfDivisors(num);
             }
         }
         return result;
     }
 
-    public static int countDivisors(int num){
+    public static int countDivisors(int num) {
         int count = 0;
 
-        for(int i = 1; i <= Math.sqrt(num); i++){
-            if(num % i == 0){
-                if(num / i == i){
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                if (num / i == i) {
                     count++;
-                }
-                else{
+                } 
+                else {
                     count += 2;
                 }
             }
@@ -29,16 +30,16 @@ class Solution {
         return count;
     }
 
-    public int sumOfDivisors(int num){
+    public int sumOfDivisors(int num) {
         int sum = 0;
 
-        for(int i = 1; i <= Math.sqrt(num); i++){
-            if(num % i == 0){
-                if(num / i == i){
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                if (num / i == i) {
                     sum += i;
-                }
-                else{
-                    sum += i + num/i;
+                } 
+                else {
+                    sum += i + num / i;
                 }
             }
         }
