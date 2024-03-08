5d2769dd65c31cbdd453afb4518cecde814db345
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-03-08 14:57:03 +0530

    Time: 51 ms (45.05%) | Memory: 44.8 MB (43.80%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int sumFourDivisors(int[] arr) {
+        int result = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            int num = arr[i];
+            int divisorCount = countDivisors(arr[i]);
+            
+            if(divisorCount == 4){
+                result += sumOfDivisors(num);
+            }
+        }
+        return result;
+    }
+
+    public static int countDivisors(int num){
+        int count = 0;
+
+        for(int i = 1; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                if(num / i == i){
+                    count++;
+                }
+                else{
+                    count += 2;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int sumOfDivisors(int num){
+        int sum = 0;
+
+        for(int i = 1; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                if(num / i == i){
+                    sum += i;
+                }
+                else{
+                    sum += i + num/i;
+                }
+            }
+        }
+        return sum;
+    }
+}
