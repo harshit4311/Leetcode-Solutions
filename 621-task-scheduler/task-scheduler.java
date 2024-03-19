ef5a8334e6fe160652543de425c214bcab7545b8
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-03-19 23:57:42 +0530

    Time: 2 ms (99.12%) | Memory: 46 MB (28.66%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < tasks.length; i++) {
+            freq[tasks[i] - 'A']++;
+        }
+
+        Arrays.sort(freq);
+
+        int chunk = freq[25] - 1;
+        int idle = chunk * n;
+
+        for (int i = 24; i >= 0; i--) {
+            if (chunk > freq[i]) {
+                idle -= freq[i];
+            } 
+            else {
+                idle -= chunk;
+            }
+        }
+
+        if (idle < 0) return tasks.length;
+        else return tasks.length + idle;
+        
+    }
+}
