b3718c7e582171a7e667f83103298ba749f3f198
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-04-03 12:19:20 +0530

    Time: 148 ms (54.92%) | Memory: 41.1 MB (91.99%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,43 +1,33 @@
 class Solution {
-    int l, m, n;
-    int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private boolean helper(char[][] arr, String word, int i, int j, int idx, int row, int col, int[][] visited) {
+        if (i < 0 || j < 0 || i >= row || j >= col || visited[i][j] == 1) return false;
+        if (word.charAt(idx) != arr[i][j]) return false;
+        if (idx == word.length() - 1) return true;
+ 
+        visited[i][j] = 1;
+        if (helper(arr, word, i, j + 1, idx + 1, row, col, visited) ||
+            helper(arr, word, i + 1, j, idx + 1, row, col, visited) ||
+            helper(arr, word, i - 1, j, idx + 1, row, col, visited) ||
+            helper(arr, word, i, j - 1, idx + 1, row, col, visited))
+            return true;
+        
+        visited[i][j] = '$';
+        return false;
+    }
 
     public boolean exist(char[][] board, String word) {
-        m = board.length;
-        n = board[0].length;
-        l = word.length();
+        int row = board.length;
+        int col = board[0].length;
+        int[][] visited = new int[row][col];
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(board[i][j] == word.charAt(0) && find(board , i, j, word, 0)) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (helper(board, word, i, j, 0, row, col, visited)) {
                     return true;
                 }
             }
         }
         return false;
     }
-
-    public boolean find(char[][] board, int i, int j, String word, int index) {
-        if(index >= word.length()) 
-        return true;
-
-        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word.charAt(index)) 
-        return false;
-
-        char temp = board[i][j];
-        board[i][j] = '$';
-
-        for(int d = 0; d < directions.length; d++) {
-            int new_i = i + directions[d][0];
-            int new_j = j + directions[d][1];
-
-            if(find(board, new_i, new_j, word, index + 1)) {
-                return true;
-            }
-        }
-
-        board[i][j] = temp;
-        return false;
-    }
-
+    
 }
