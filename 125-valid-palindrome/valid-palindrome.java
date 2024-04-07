223599b653adbf6578c028cc5034077073d02446
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-04-07 16:16:33 +0530

    Time: 8 ms (45.66%) | Memory: 57 MB (5.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,29 +1,25 @@
 class Solution {
-   public boolean isPalindrome(String s) {
-        if (s.isEmpty()) return true;
+    public boolean isPalindrome(String string) {
+        return isPalindrome(string, 0, string.length() - 1);
+    }
 
-        int left = 0;
-        int right = s.length() - 1;
+    public boolean isPalindrome(String string, int start, int end) {
+        if (start >= end) return true;
 
-        while (left < right) {
-            char leftChar = s.charAt(left);
-            char rightChar = s.charAt(right);
+        char startChar = Character.toLowerCase(string.charAt(start));
+        char endChar = Character.toLowerCase(string.charAt(end));
 
-            if (!Character.isLetterOrDigit(leftChar)) {
-                left++;
-            } 
-            else if (!Character.isLetterOrDigit(rightChar)) {
-                right--;
-            } 
-            else {
-                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
-                    return false;
-                }
-                left++;
-                right--;
-            }
+        // Skip non-alphanumeric characters
+        if (!Character.isLetterOrDigit(startChar)) {
+            return isPalindrome(string, start + 1, end);
+        }
+        if (!Character.isLetterOrDigit(endChar)) {
+            return isPalindrome(string, start, end - 1);
         }
 
-        return true;
+        // Compare characters
+        if (startChar != endChar) return false;
+
+        return isPalindrome(string, start + 1, end - 1);
     }
 }
