c2fce3496c344ab58016d7bfa12e730fa9c5fca8
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-04-11 20:28:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (70.97%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -4,16 +4,17 @@ class Solution {
         return helper(m, n, 0, 0, dp);
     }
 
-    public int helper(int m, int n, int i, int j, int[][] dp) {
+    public static int helper(int m, int n, int i, int j, int[][] dp) {
         // Base Cases
         if(i >= m || j >= n) return 0;
-        if(i == m - 1 && j == n - 1) return 1;
+        if(i == m -1 || j == n -1) return 1; // This is the last row/column and there's just 1 option left
 
         if(dp[i][j] != 0) {
             return dp[i][j];
         }
-        
+
         dp[i][j] = helper(m, n, i + 1, j, dp) + helper(m, n, i, j + 1, dp);
+
         return dp[i][j];
     }
 }
