8b054b8280f29d0ac63a41d741593fc286eebd30
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-04-16 19:08:00 +0530

    Time: 10 ms (90.88%) | Memory: 41.2 MB (95.44%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,25 +1,17 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        int[] dp = new int[t.length() + 1];
+        dp[0] = 1; // Base case: when t is empty, there is one subsequence in s
         
-        // Base Case : When t is empty, there is one subsequence in s
-        for(int i = 0; i <= s.length(); i++) {
-            dp[i][0] = 1;
-        }
-        
-        for(int i = 1; i <= s.length(); i++) {
-            for(int j = 1; j <= t.length(); j++) {
-
-                // If characters match, we can either include or exclude the character from s
-                if(s.charAt(i - 1) == t.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                } 
-                else { // If characters don't match, we can only exclude the character from s
-                    dp[i][j] = dp[i - 1][j];
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = t.length(); j > 0; j--) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    // If characters match, we can either include or exclude the character from s
+                    dp[j] += dp[j - 1];
                 }
             }
         }
         
-        return dp[s.length()][t.length()];
+        return dp[t.length()];
     }
 }
