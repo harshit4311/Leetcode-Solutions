423d0b28b3827ddcb4947c4bc13444fb39472026
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-04-24 18:58:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (34.79%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,32 +3,27 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
-        // Dynamic programming array to store the number of unique paths
         int[][] dp = new int[m][n];
 
-        // Initialize DP array
-        for (int i = 0; i < m; i++) {
+        // Initialize DP array as -1 for all cell values 
+        for(int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 dp[i][j] = -1;
             }
         }
 
-        // Start recursion from the top-left corner
         return helper(0, 0, m, n, dp, obstacleGrid);
     }
 
     public int helper(int i, int j, int m, int n, int[][] dp, int[][] obstacleGrid) {
         // Check for out-of-bounds
-        if (i >= m || j >= n) return 0;
+        if(i >= m || j >= n) return 0;
 
         // If reached the destination (bottom-right corner)
-        if (i == m - 1 && j == n - 1) {
-            // If there's an obstacle at the destination, return 0
-            if (obstacleGrid[i][j] == 1) {
-                return 0;
-            } else {
-                return 1;
-            }
+        if(i == m - 1 && j == n - 1) {
+            // If there's an obstacle at the destination(end), return 0, else return 1
+            if (obstacleGrid[i][j] == 1) return 0; 
+            else return 1;
         }
 
         // If already calculated, return the value from DP array
@@ -40,11 +35,9 @@ class Solution {
             return 0;
         }
 
-        // Calculate the number of unique paths recursively
         int right = helper(i + 1, j, m, n, dp, obstacleGrid);
         int down = helper(i, j + 1, m, n, dp, obstacleGrid);
 
-        // Store the calculated value in DP array
         dp[i][j] = right + down;
 
         return dp[i][j];
