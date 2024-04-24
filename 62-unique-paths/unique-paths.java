09e1a778016a58dcdea5c618670a177e940293f5
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-04-24 17:49:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (56.53%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,27 +1,37 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-    
-        // Initially, fill the DP array with -1
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+
+        // Fill the dp array with -1
+        for(int i = 0; i <= m - 1; i++) {
+            for(int j = 0; j <= n - 1; j++) {
                 dp[i][j] = -1;
             }
-        } 
+        }
 
         return helper(0, 0, m, n, dp);
     }
 
     public int helper(int i, int j, int m, int n, int[][] dp) {
-        if(i == m - 1 && j == n - 1) return 1; // We reached the end
-        if(i >=m || j >= n) return 0; // Index out of bounds
 
-        if(dp[i][j] != -1) return dp[i][j];
+        // We reached the last cell
+        if(i == m - 1 && j == n - 1) { 
+            return 1;
+        }  
+
+        // Array index out of bounds
+        if(i >= m || j >= n) { 
+            return 0;
+        }
+
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
 
         int down = helper(i + 1, j, m, n, dp);
         int right = helper(i, j + 1, m, n, dp);
 
-        dp[i][j] = down + right; // Memoize(Store) the result
+        dp[i][j] = down + right;
 
         return dp[i][j];
     }
