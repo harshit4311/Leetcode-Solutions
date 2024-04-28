ea857c798a6afe82fef93131c886624521b3d095
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-04-28 15:34:33 +0530

    Time: 2 ms (84.08%) | Memory: 44.1 MB (95.81%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -3,26 +3,26 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
         
-        backtrack(arr, target, 0, current, result);
+        helper(0, arr, target, current, result);
         return result;
     }
 
-    public void backtrack(int[] arr, int target, int index, List<Integer> current, List<List<Integer>> result) {
-        if (target == 0) {
+    public void helper(int i, int[] arr, int target, List<Integer> current, List<List<Integer>> result) {
+        if(target == 0) {
             result.add(new ArrayList<>(current));
             return;
         }
 
-        if (target < 0 || index == arr.length) {
+        if(target < 0 || i == arr.length) {
             return;
         }
 
         // Case 1: Include current element
-        current.add(arr[index]);
-        backtrack(arr, target - arr[index], index, current, result);
+        current.add(arr[i]);
+        helper(i, arr, target - arr[i], current, result);
         current.remove(current.size() - 1);
 
         // Case 2: Exclude current element and move to next
-        backtrack(arr, target, index + 1, current, result);
+        helper(i + 1, arr, target, current, result);
     }
 }
