ab967dcc4e05be3b3a990623f0ac4d1018c7e28b
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-04-28 22:51:14 +0530

    Time: 51 ms (10.39%) | Memory: 55.3 MB (52.47%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,41 +1,52 @@
-class TrieNode{
+import java.util.*;
+
+class TrieNode {
     HashMap<Character, TrieNode> map;
     boolean isEnd;
 
-    public TrieNode(){
+    public TrieNode() {
         map = new HashMap<Character, TrieNode>();
         isEnd = false;
     }
 }
 
-public class Trie{
+class Trie {
     public TrieNode root;
 
-    public Trie(){
-        root = new TrieNode();    
+    public Trie() {
+        root = new TrieNode();
     }
     
     public void insert(String word) {
-    TrieNode current = root;
-    for (int i = 0; i < word.length(); i++) {
-        char ch = word.charAt(i);
+        TrieNode current = root;
 
-        if (!current.map.containsKey(ch)) {
-            current.map.put(ch, new TrieNode());
+        for(int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            // Case 1: If character 'ch' is not present 
+            if(!current.map.containsKey(ch)) {
+                current.map.put(ch, new TrieNode());
+            }
+
+            // Case 2: If character 'ch' is present
+            current = current.map.get(ch); 
         }
-        current = current.map.get(ch);
+        current.isEnd = true;
     }
-    current.isEnd = true;
-}
     
     public boolean search(String word) {
         TrieNode current = root;
-        for(int i = 0; i < word.length(); i++){
+        
+        for(int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            
-            if(!current.map.containsKey(ch)){
+
+            // Does not contain character, so will not contain the String 'word'
+            if(!current.map.containsKey(ch)) {
                 return false;
             }
+
+
+            // Move to the next character index
             current = current.map.get(ch);
         }
         return current.isEnd;
@@ -43,14 +54,18 @@ public class Trie{
     
     public boolean startsWith(String prefix) {
         TrieNode current = root;
-        for(int i = 0; i < prefix.length(); i++){
+
+        for(int i = 0; i < prefix.length(); i++) {
             char ch = prefix.charAt(i);
 
-            if(!current.map.containsKey(ch)){
+            // Does not start with ch
+            if(!current.map.containsKey(ch)) {
                 return false;
             }
+
+            // Move to the next character
             current = current.map.get(ch);
-        }
+        } 
         return true;
     }
 }
