48a88aedda461cf165bccc762e0a31dd5758a87e
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-05-14 13:30:34 +0530

    Time: 3 ms (82.57%) | Memory: 42.9 MB (79.91%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] arr, int target) {
+        Arrays.sort(arr);
+        
+        List<List<Integer>> list = new ArrayList<>();
+
+        helper(0, target, arr, list, new ArrayList<>());
+        return list;
+    }
+
+    public void helper(int i, int target, int[] arr, List<List<Integer>> list, List<Integer> ds) {
+        if(i == arr.length) {
+            if(target == 0) {
+                list.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+
+        // Take 
+        if(arr[i] <= target) {
+            ds.add(arr[i]);
+            helper(i + 1, target - arr[i], arr, list, ds);
+            ds.remove(ds.size() - 1);
+        }
+        
+        // Skip all duplicates
+        while(i + 1 < arr.length && arr[i] == arr[i + 1]) {
+            i++;
+        }
+
+        // Don't Take
+        helper(i + 1, target, arr, list, ds);
+    }
+}
