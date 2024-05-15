d4a646a3f11b5e8647906a80afc19a4514715892
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-05-15 14:22:36 +0530

    Time: 2 ms (47.44%) | Memory: 44.2 MB (84.98%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,31 +2,29 @@ class Solution {
     public List<List<Integer>> permute(int[] arr) {
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> ds = new ArrayList<>();
-        boolean[] freq = new boolean[arr.length];
 
-        helper(arr, list, ds, freq);
+        HashSet<Integer> visited = new HashSet<>();
+
+        helper(arr, list, ds,visited);
         return list;
     }
 
-    public void helper(int[] arr, List<List<Integer>> list, List<Integer> ds, boolean[] freq) {
+    public void helper(int[] arr, List<List<Integer>> list, List<Integer> ds, HashSet<Integer> visited) {
         if(ds.size() == arr.length) {
             list.add(new ArrayList<>(ds));
-            return;
-        } // Backtrack
-
-        for(int i = 0; i < arr.length; i++) {
-
-            // If not marked as 'added' or 'true' in our 'ds'
-            if(!freq[i]) {
-                freq[i] = true; // Mark this is added in the 'ds'
-
-                ds.add(arr[i]); // Add in the 'ds'
-                helper(arr, list, ds, freq); // Op
-
-                ds.remove(ds.size() - 1); // Remove from the 'ds'
-                freq[i] = false; // Mark it as not added
-            }
+            return; // Backtrack
         }
 
+        for(int i = 0; i < arr.length; i++) {
+            if(!visited.contains(i)) {
+                visited.add(i);
+                ds.add(arr[i]);
+
+                helper(arr, list, ds, visited);
+                
+                ds.remove(ds.size() - 1);
+                visited.remove(i);
+            }
+        }
     }
 }
