7d46cb6c4e8424bf847bb10beb2f26454b2f337d
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-05-22 14:17:28 +0530

    Time: 2 ms (98.16%) | Memory: 49.3 MB (64.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,26 @@
-class Pair {
-    int r;
-    int c;
-
-    Pair(int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
-}
-
 class Solution {
     int m;
     int n;
-    boolean[][] visited;
-    int islandCount = 0;
-    int[] d_row = {-1, 0, 1, 0};
-    int[] d_col = {0, -1, 0, 1};
 
     public int numIslands(char[][] grid) {
+
+        // 0 Islands
+        if(grid == null || grid.length == 0) {
+            return 0; 
+        }
+
+        int islandCount = 0;
+
         m = grid.length;
         n = grid[0].length;
-        visited = new boolean[m][n];
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == '1' && !visited[i][j]) {
-                    bfs(grid, i, j);
+
+                // Found a new island
+                if(grid[i][j] == '1') {
                     islandCount++;
+                    dfs(grid, i, j);
                 }
             }
         }
@@ -33,28 +28,16 @@ class Solution {
         return islandCount;
     }
 
-    public void bfs(char[][] grid, int r, int c) {
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(r, c));
-        visited[r][c] = true;
-
-        while(!queue.isEmpty()) {
-            Pair current = queue.poll();
-            int cur_row = current.r; // Get the row index of the current cell
-            int cur_col = current.c; // Get the column index of the current cell
-
-            // Explore all four possible directions
-            for(int i = 0; i < 4; i++) {
-                int new_row = cur_row + d_row[i];
-                int new_col = cur_col + d_col[i];
-
-                // Check if the new cell is within the grid bounds, unvisited, and part of the island
-                if(new_row >= 0 && new_row < m && new_col >= 0 && new_col < n && 
-                    !visited[new_row][new_col] && grid[new_row][new_col] == '1') {
-                    queue.add(new Pair(new_row, new_col));
-                    visited[new_row][new_col] = true;
-                }
-            }
+    public void dfs(char[][] grid, int i, int j) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1') {
+            return; // Backtrack
         }
+
+        grid[i][j] = '0'; // Mark as visited
+
+        dfs(grid, i - 1, j); // Up
+        dfs(grid, i, j + 1); // Right
+        dfs(grid, i + 1, j); // Down
+        dfs(grid, i, j - 1); // Left
     }
 }
