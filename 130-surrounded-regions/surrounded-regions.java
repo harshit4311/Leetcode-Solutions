17a4f18d8a8b7206ea438163d78cc0cfc4a56270
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-05-22 17:02:48 +0530

    Time: 2 ms (86.34%) | Memory: 44.4 MB (99.77%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,3 +1,13 @@
+class Pair {
+    int row;
+    int col;
+
+    public Pair(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
 class Solution {
     int m;
     int n;
@@ -11,17 +21,20 @@ class Solution {
         visited = new boolean[m][n];
 
         // Traverse over the borders using BFS
+
+        // Traverse over Rows
         for (int i = 0; i < m; i++) {
-            bfs(i, 0, board);
-            bfs(i, n - 1, board);
+            bfs(i, 0, board); // Traverse over First Row
+            bfs(i, n - 1, board); // Traverse over Last Row
         }
 
-        for (int j = 0; j < n; j++) {
-            bfs(0, j, board);
-            bfs(m - 1, j, board);
+        // Traverse over Columns
+        for (int i = 0; i < n; i++) {
+            bfs(0, i, board); // Traverse over First Column
+            bfs(m - 1, i, board); // Traverse over Last Column
         }
 
-        // Convert 'O's that are not visited to 'X's
+        // Convert all 'O's that are not visited to 'X'
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (!visited[i][j] && board[i][j] == 'O') {
@@ -29,26 +42,27 @@ class Solution {
                 }
             }
         }
+
     }
 
     public void bfs(int row, int col, char[][] board) {
-        if (board[row][col] == 'O' && !visited[row][col]) {
-            Queue<int[]> queue = new LinkedList<>();
-            queue.add(new int[]{row, col});
+        if(board[row][col] == 'O' && !visited[row][col]) {
+            Queue<Pair> queue = new LinkedList<>();
+            queue.add(new Pair(row, col));
             visited[row][col] = true;
 
             while (!queue.isEmpty()) {
-                int[] current = queue.poll();
-                int r = current[0];
-                int c = current[1];
+                Pair current = queue.poll();
+                int r = current.row;
+                int c = current.col;
 
-                for(int i = 0; i < 4; i++) {
+                for (int i = 0; i < 4; i++) {
                     int newRow = r + d_row[i];
                     int newCol = c + d_col[i];
 
-                    if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n &&
+                    if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n &&
                             !visited[newRow][newCol] && board[newRow][newCol] == 'O') {
-                        queue.offer(new int[]{newRow, newCol});
+                        queue.add(new Pair(newRow, newCol));
                         visited[newRow][newCol] = true;
                     }
                 }
