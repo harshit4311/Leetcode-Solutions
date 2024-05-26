29ceb488274a2f129ebf80c19ae8ff7120be43a9
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-05-26 17:36:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (45.74%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -5,24 +5,26 @@ class Solution {
         int n = graph.length;
         color = new int[n];
 
+        // Fill all cells with -1
         for(int i = 0; i < n; i++) {
             color[i] = -1;
         }
 
+        // Check if the graph is Bipartite
         for(int i = 0; i < n; i++) {
             if(color[i] == -1) {
                 if(DFS(i, graph, 0) == false) {
                     return false;
-                }
-            }
+                } 
+            } 
         }
 
         return true;
     }
 
     public boolean DFS(int node, int[][] graph, int currentColor) {
-        color[node] = currentColor;  // Color the node with the currentColor
-        
+        color[node] = currentColor; // Fill the first node with currentColor
+
         for(int i = 0; i < graph[node].length; i++) {
             int neighbor = graph[node][i];
 
@@ -30,8 +32,10 @@ class Solution {
                 if(!DFS(neighbor, graph, 1 - currentColor)) {
                     return false;
                 }
-
             } 
+
+            // If both, Neighbor and CurrentNode have the same color,
+            // Graph is NOT Bipartite
             else if(color[neighbor] == color[node]) {
                 return false;
             }
