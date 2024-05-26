f7760e8ab4e66b3abaf91d69cb0c128775aa169a
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-05-26 17:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (83.02%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,20 +1,17 @@
 class Solution {
-    int n;
     int[] color;
 
     public boolean isBipartite(int[][] graph) {
-        n = graph.length;
+        int n = graph.length;
         color = new int[n];
 
-        // Fill all nodes with -1, depicting--> uncolored nodes
         for(int i = 0; i < n; i++) {
             color[i] = -1;
         }
 
-        // Check if it's a bipartite graph
         for(int i = 0; i < n; i++) {
             if(color[i] == -1) {
-                if(BFS(i, graph) == false) {
+                if(DFS(i, graph, 0) == false) {
                     return false;
                 }
             }
@@ -23,31 +20,23 @@ class Solution {
         return true;
     }
 
-    public boolean BFS(int start, int[][] graph) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        color[start] = 0;  // Start coloring the nodes with 0 
+    public boolean DFS(int node, int[][] graph, int currentColor) {
+        color[node] = currentColor;  // Color the node with the currentColor
+        
+        for(int i = 0; i < graph[node].length; i++) {
+            int neighbor = graph[node][i];
 
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-
-            for(int i = 0; i < graph[node].length; i++) { // graph[node] gives you the list of nodes that are directly connected to node
-            // graph[0] returns {1, 3}. That means nodes -> 1 and 3 are connected to node -> 0
-
-                int neighbor = graph[node][i]; // Access the 'i'th neighbor of the node
-
-                // If the neighbor has not been colored, color it with the opposite color
-                if(color[neighbor] == -1) {
-                    color[neighbor] = 1 - color[node];
-                    queue.add(neighbor);
-                } 
-
-                // If 2 neighbors have the same color
-                else if(color[neighbor] == color[node]) { 
+            if(color[neighbor] == -1) {
+                if(!DFS(neighbor, graph, 1 - currentColor)) {
                     return false;
                 }
+
+            } 
+            else if(color[neighbor] == color[node]) {
+                return false;
             }
         }
+
         return true;
     }
 }
