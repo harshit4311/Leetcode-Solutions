0241ff2a3dabd1cbecf8379fc38fff92b346e72a
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-01 16:43:21 +0530

    Time: 1 ms (90.06%) | Memory: 48.2 MB (10.62%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,30 +1,69 @@
+class UnionFind {
+    int[] parent;
+    int[] rank;
+    int count;
+
+    public UnionFind(int size) {
+        parent = new int[size];
+        rank = new int[size];
+        count = size;
+
+        for(int i = 0; i < size; i++) {
+            parent[i] = i; // Initially, parent of each node is the node itself
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int p) {
+        if(parent[p] != p) {
+            parent[p] = find(parent[p]);
+        }
+
+        return parent[p];
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p); // Find the root of P
+        int rootQ = find(q); // Find the root of Q
+
+        if(rootP != rootQ) {
+            if(rank[p] > rank[q]) {
+                parent[rootQ] = rootP;
+            }
+
+            else if(rank[p] < rank[q]) {
+                parent[rootP] = rootQ;
+            }
+
+            else { // Ranks of poth, P and Q are the same 
+                parent[rootQ] = rootP;
+                rank[rootP]++;
+            }
+
+            count--;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
 
-        boolean[] isVisited = new boolean[n];
-
-        int count = 0;
+        UnionFind uf = new UnionFind(n);
 
         for(int i = 0; i < n; i++) {
-            if(!isVisited[i]) {
-                count++;
-                DFS(i, isVisited, isConnected);
+            for(int j = i + 1; j < n; j++) {
+                if(isConnected[i][j] == 1) {
+                    uf.union(i, j);
+                }
             }
         }
 
-        return count;
-    }
-
-    public void DFS(int start, boolean[] isVisited, int[][] isConnected) {
-        isVisited[start] = true;
-
-        for(int i = 0; i < isConnected.length; i++) {
-
-             // If there is a connection between cities and the city has not been visited yet
-            if(isConnected[start][i] == 1 && !isVisited[i]) {
-                DFS(i, isVisited, isConnected);
-            }
-        }
+        return uf.getCount();
     }
 }
