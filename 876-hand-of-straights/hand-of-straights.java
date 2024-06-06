08c01cf1d2cd6f8d35f6bfa99b2bdca973d8e289
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-06 23:59:06 +0530

    Time: 39 ms (40.56%) | Memory: 45.8 MB (27.26%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+
+        if(hand.length % groupSize != 0) {
+            return false;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // Add all elements to the PQ
+        for(int i = 0; i< hand.length; i++) {
+            pq.add(hand[i]);
+        }
+
+        while(!pq.isEmpty()) {
+            int start = pq.poll(); // Get the smallest card to start a group
+
+            for(int i = 1; i < groupSize; i++) {
+                if(!pq.remove(start + i)) {
+                    return false; // If we can't find the next consecutive card, return false
+                }
+            }
+        }
+
+        return true;
+    }
+}
