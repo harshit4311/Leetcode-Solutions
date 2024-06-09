e284ad288925e771614171536a1c25d49f12ffd4
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-09 22:35:44 +0530

    Time: 4 ms (95.15%) | Memory: 44.7 MB (62.02%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,46 +1,42 @@
-
 class MinStack {
-    private Stack<Integer> stack;
-    private Stack<Integer> minStack;
-
-    // Initializes the stack object
+    Deque<Integer> stack;
+    Deque<Integer> minQueue;
+    
     public MinStack() {
-        stack = new Stack<>();
-        minStack = new Stack<>();
+        stack = new ArrayDeque<>();
+        minQueue = new ArrayDeque<>();
     }
-
-    // Pushes the element val onto the stack
+    
     public void push(int val) {
         stack.push(val);
-        if (minStack.isEmpty() || val <= minStack.peek()) {
-            minStack.push(val);
+
+        if(minQueue.isEmpty() || !minQueue.isEmpty() && val <= minQueue.getFirst()) {
+            minQueue.push(val);
         }
     }
-
-    // Removes the element on the top of the stack
+    
     public void pop() {
-        if (stack.isEmpty()) {
-            return;
+        if(!minQueue.isEmpty() && stack.peek() <= minQueue.getFirst()) {
+            minQueue.pop();
         }
-        int removedElement = stack.pop();
-        if (removedElement == minStack.peek()) {
-            minStack.pop();
-        }
-    }
 
-    // Gets the top element of the stack
+        stack.pop();
+    }
+    
     public int top() {
-        if (stack.isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
-        }
         return stack.peek();
     }
-
-    // Retrieves the minimum element in the stack
+    
     public int getMin() {
-        if (minStack.isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
-        }
-        return minStack.peek();
+        return minQueue.getFirst();
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
