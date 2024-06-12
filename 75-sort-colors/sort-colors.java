4fed9619b81f2d266af073c8a5a66bab3da1400d
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-12 21:19:36 +0530

    Time: 1 ms (21.51%) | Memory: 41.6 MB (60.77%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,7 +1,37 @@
 class Solution {
     public void sortColors(int[] arr) {
-        Arrays.sort(arr);
+        int n = arr.length;
+        
+        // <color, frequency> map
+        HashMap<Integer, Integer> colorCount = new HashMap<>();
 
-        return;
+        // Initialising the map with count = 0 for all colors
+        colorCount.put(0, 0);
+        colorCount.put(1, 0);
+        colorCount.put(2, 0);
+
+        // Count the occurences of each color 
+        for(int i = 0; i < arr.length; i++) {
+            int number = arr[i];
+            colorCount.put(number, colorCount.get(number) + 1);
+        }
+
+        int index = 0;
+
+        // Add all 0's in the array
+        for (int i = 0; i < colorCount.get(0); i++) {
+            arr[index++] = 0;
+        }
+        
+        // Add all 1's in the array
+        for (int i = 0; i < colorCount.get(1); i++) {
+            arr[index++] = 1;
+        }
+        
+        // Add all 2's in the array
+        for (int i = 0; i < colorCount.get(2); i++) {
+            arr[index++] = 2;
+        }
+
     }
 }
