3d26045c829d448aa5ace212faf4a432568c4105
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-13 23:16:57 +0530

    Time: 3 ms (62.42%) | Memory: 44.2 MB (34.48%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -5,6 +5,8 @@ class Solution {
 
         int count = 0;
 
+        // Count the difference in the positions of each seat and student 
+        // Increment the count for each differnece in positions
         for(int i = 0; i < seats.length; i++) {
             count += Math.abs(students[i] - seats[i]);
         }
