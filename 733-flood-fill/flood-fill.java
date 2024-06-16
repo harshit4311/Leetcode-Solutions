658637ddef0ef052d99a4873682298f869d316c6
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-16 14:35:33 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (96.11%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,41 +1,34 @@
 class Solution {
-    public int[][] floodFill(int[][] grid, int sr, int sc, int newColor) {
-        
-        // Find initialColor by checking the cell-> grid[sr][sc] 
+    public int[][] floodFill(int[][] grid, int sr, int sc, int color) {
         int initialColor = grid[sr][sc];
-        
-        // If the initial color is the same as the new color, we will return the same(original grid) as no operation is to be performed
-        if(initialColor == newColor) {
+
+        if(initialColor == color) {
             return grid;
         }
 
-        // Directions for moving up, right, down, and left
         int[] d_row = {-1, 0, 1, 0};
         int[] d_col = {0, 1, 0, -1};
 
-        dfs(sr, sc, initialColor, newColor, grid, d_row, d_col);
+        helperDFS(sr, sc, initialColor, color, d_row, d_col, grid);
         return grid;
     }
 
-    public void dfs(int sr, int sc, int initialColor, int newColor, int[][] grid, int[] d_row, int[] d_col) {
+    public void helperDFS(int sr, int sc, int initialColor, int color, int[] d_row, int[] d_col, int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
 
-        // Fill the current cell with newColor
-        grid[sr][sc] = newColor;
+        // Fill the cell with the newColor that has to be appplied to all cells
+        grid[sr][sc] = color; 
 
-        // Traverse in all 4 directions
         for(int i = 0; i < 4; i++) {
-            int new_row = sr + d_row[i]; // New Row Index
-            int new_col = sc + d_col[i]; // New Column Index
+            int newRow = sr + d_row[i];
+            int newCol = sc + d_col[i];
 
-            // Check if new Row & Column indices are within the boundaries 
-            // and whether the current cell is still filled with the initialColor
-            // If it is still filled with initialColor, fill newColor in it using DFS
-            if(new_row >= 0 && new_row <= m - 1 && new_col >= 0 && new_col <= n - 1
-                && grid[new_row][new_col] == initialColor) {
-                    dfs(new_row, new_col, initialColor, newColor, grid, d_row, d_col);
-                }
+            if(newRow >= 0 && newRow <= m - 1 && newCol >= 0 && newCol <= n - 1  
+                && grid[newRow][newCol] == initialColor) {
+                    helperDFS(newRow, newCol, initialColor, color, d_row, d_col, grid);
+            }
         }
+
     }
 }
