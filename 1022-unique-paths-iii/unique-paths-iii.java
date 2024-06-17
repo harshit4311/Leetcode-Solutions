1668d859d734c1b7d5c0d8bdc39ce91d4504438b
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-18 01:31:53 +0530

    Time: 1 ms (39.69%) | Memory: 40.7 MB (69.43%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.java
+++ 1022-unique-paths-iii/unique-paths-iii.java
@@ -2,16 +2,16 @@ class Solution {
     int m;
     int n;
     int result;
-    int nonObstacles;
+    int nonObstacleCount;
     
     int[][] directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
     
     public int uniquePathsIII(int[][] grid) {
         m = grid.length;
         n = grid[0].length;
-        
+
         result = 0;
-        nonObstacles = 0;
+        nonObstacleCount = 0;
         
         int start_i = 0;
         int start_j = 0;
@@ -19,30 +19,30 @@ class Solution {
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] == 0) {
-                    nonObstacles++;
+                    nonObstacleCount++;
                 }
                 
                 if(grid[i][j] == 1) {
                     start_i = i;
                     start_j = j;
-                    nonObstacles++; // Counting starting point as nonObstacle
+                    nonObstacleCount++; // Counting starting point as nonObstacle
                 }
             }
         }
         
-        int count = 0;
+        int visitedCount = 0;
         
-        helper(start_i, start_j, count, grid);
+        helper(start_i, start_j, visitedCount, grid);
         return result;
     }
     
-    public void helper(int i, int j, int count, int[][] grid) {
+    public void helper(int i, int j, int visitedCount, int[][] grid) {
         if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1) {
             return;
         }
         
         if(grid[i][j] == 2) {
-            if(count == nonObstacles) {
+            if(visitedCount == nonObstacleCount) {
                 result++;
             }
             return;
@@ -51,14 +51,14 @@ class Solution {
         // Mark cell as visited
         int temp = grid[i][j];
         grid[i][j] = -1;
-        count++;
+        visitedCount++;
         
         // Explore in all 4 directions
         for(int d = 0; d < directions.length; d++) {
             int new_i = i + directions[d][0];
             int new_j = j + directions[d][1];
             
-            helper(new_i, new_j, count, grid);
+            helper(new_i, new_j, visitedCount, grid);
         }
         
         // Backtrack
