5c33d2db48048367eb08a366c05b00ad9bfde0c9
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-19 22:59:28 +0530

    Time: 20 ms (62.40%) | Memory: 58.1 MB (35.67%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int minDays(int[] arr, int m, int k) {
+        int n = arr.length;
+
+        if (m * k > n) {
+            // If the total number of flowers needed is greater than available flowers
+            return -1;
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            end = Math.max(end, arr[i]);
+        }
+
+        int minDaysResult = -1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(canMakeBouquets(arr, mid, k) >= m) {
+                minDaysResult = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return minDaysResult;
+    }
+
+    public int canMakeBouquets(int[] arr, int mid, int k) {
+        int bouquetCount = 0;
+        int consecutiveFlowersCount = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] <= mid) {
+                consecutiveFlowersCount++;
+            } else {
+                consecutiveFlowersCount = 0;
+            }
+
+            if(consecutiveFlowersCount == k) {
+                bouquetCount++;
+                consecutiveFlowersCount = 0;
+            }
+        }
+
+        return bouquetCount;
+    }
+}
