6f92f427dd22a7fafd2f0c39fb20317e67fb26ff
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-06-29 00:07:28 +0530

    Time: 7 ms (87.57%) | Memory: 62.1 MB (88.11%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long maximumImportance(int n, int[][] roads) {
+        int[] degree = new int[n];
+
+        for(int[] road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+
+        // assign larger value to larger degree node
+        Arrays.sort(degree);
+        long result = 0;
+
+        for(int i = 0; i < n; i++) {
+            result += (long) degree[i] * (i + 1);
+        }
+        
+        return result;
+    }
+}
