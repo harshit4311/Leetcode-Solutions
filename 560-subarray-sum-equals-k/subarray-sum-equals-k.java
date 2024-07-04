339e0650fddfa013e9ddfd891ea6968388887030
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-07-04 23:51:31 +0530

    Time: 23 ms (91.53%) | Memory: 46.4 MB (39.79%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -7,15 +7,16 @@ class Solution {
 
         map.put(0, 1);
 
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             sum += arr[i];
 
-            if (map.containsKey(sum - k)) {
+            if(map.containsKey(sum - k)) {
                 count += map.get(sum - k);
             }
+
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        
+         
         return count;
     }
 }
