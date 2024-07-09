2f08b238148d2cd9d6b669fb04398a155632ef3e
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-07-10 00:27:48 +0530

    Time: 3 ms (93.06%) | Memory: 72.7 MB (44.08%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        int n = customers.length;
+
+        int currentTime = 0;
+        double total_waiting_time = 0;
+
+        for(int i = 0; i < n; i++) {
+            int arrivalTime = customers[i][0];
+            int timeNeeded = customers[i][1];
+
+            if(currentTime < arrivalTime) {
+                currentTime = arrivalTime;
+            }
+
+            currentTime += timeNeeded;
+            int waitingTime_perOrder = currentTime - arrivalTime;
+            total_waiting_time += waitingTime_perOrder;
+        }
+
+        return (double) total_waiting_time / n;
+    }
+}
