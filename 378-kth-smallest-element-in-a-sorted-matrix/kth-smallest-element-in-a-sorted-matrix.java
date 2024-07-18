9e693f22bafdcdecafea6c704ff7fd43c6d0767e
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-07-18 14:30:57 +0530

    Time: 23 ms (14.20%) | Memory: 48 MB (73.47%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,22 +1,22 @@
-import java.util.PriorityQueue;
-
 class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+    public int kthSmallest(int[][] A, int B) {
+        int n = A.length;
+        int m = A[0].length;
 
-        int rows = matrix.length;
-        int columns = matrix[0].length;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                pq.add(matrix[i][j]);
-
-                if (pq.size() > k) {
-                    pq.poll();
-                }
+        // Add elements to the PriorityQueue in row-wise + sorted order
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                minHeap.add(A[i][j]);
             }
         }
 
-        return pq.peek();
+        // Remove (B - 1) elements from PriorityQueue
+        for(int i = 1; i < B; i++) {
+            minHeap.poll();
+        }
+
+        return minHeap.peek();
     }
 }
