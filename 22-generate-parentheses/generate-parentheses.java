5121479d44922bea1a38486accdd3891a0599ecc
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-07-25 23:18:04 +0530

    Time: 2 ms (55.42%) | Memory: 43.7 MB (14.35%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,46 +1,23 @@
-
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
-        
-        generate(new StringBuilder(), n, 0, result);
+        List<String> result = new ArrayList<String>();
+
+        recurse(result, 0, 0, "", n);
         return result;
     }
-
-    public void generate(StringBuilder str, int n, int length, List<String> result) {
-        if(length == 2 * n) {
-            if(isValid(str)) {
-                result.add(str.toString());
-            }
+    
+    public void recurse(List<String> result, int start, int end, String s, int n) {
+        if(s.length() == n * 2) {
+            result.add(s);
             return;
         }
-
-        str.append('(');
-        generate(str, n, length + 1, result);
-        str.deleteCharAt(str.length() - 1); // Backtrack by removing the last character
-
-        str.append(')');
-        generate(str, n, length + 1, result);
-        str.deleteCharAt(str.length() - 1); // Backtrack by removing the last character
-    }
-
-    public boolean isValid(StringBuilder str) {
-        int totalSum = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if(ch == '(') {
-                totalSum++;
-            } 
-            else {
-                totalSum--;
-
-                if (totalSum < 0) {
-                    return false;
-                }
-            }
+        
+        if(start < n) {
+            recurse(result, start + 1, end, s + "(", n);
         }
-
-        return totalSum == 0;
+        
+        if(end < start) {
+            recurse(result, start, end + 1, s + ")", n);
+        }
     }
 }
