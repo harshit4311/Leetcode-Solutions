ea38fbbdde853ae33953708bfe52fcd185c7b29c
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-08-27 23:49:07 +0530

    Time: 10 ms (7.98%) | Memory: 48.7 MB (8.55%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,22 +1,24 @@
 class Solution {
-    public int findContentChildren(int[] greed, int[] size) {
-
-        if(size.length == 0) return 0;
-        
-        int count = 0;
-        Arrays.sort(greed);
-        Arrays.sort(size);
-        
-        int i = 0;
-        int j = 0;
-        
-        for (i = 0, j = 0; i < greed.length && j < size.length; j++) {
-            if (greed[i] <= size[j]) {
-                count++;
-                i++;
-            }
+    public int findContentChildren(int[] greedFactor, int[] size) {
+        if (size.length == 0) {
+            return 0;
         }
 
-        return count;
+        Arrays.sort(greedFactor);
+        Arrays.sort(size);
+
+        int left = 0;
+        int right = 0;
+
+        while (left < size.length && right < greedFactor.length) {
+            if (size[left] >= greedFactor[right]) {
+                right++;
+            }
+
+            left++;
+
+        }
+
+        return right;
     }
 }
