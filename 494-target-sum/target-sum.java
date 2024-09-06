6fb28c38b53a26452a343706e678c80afcfeabbe
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-09-06 12:42:19 +0530

    Time: 662 ms (8.18%) | Memory: 41.2 MB (74.28%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -3,22 +3,21 @@ class Solution {
 
     public int findTargetSumWays(int[] arr, int target) {
         helper(arr, 0, 0, target);
-        return count;
+
+        return count;    
     }
 
     public int helper(int[] arr, int i, int currentSum, int target) {
         if(i == arr.length) {
             if(currentSum == target) {
                 count++;
-            }
+            } 
         }
-
         else {
-            helper(arr, i + 1, currentSum - arr[i], target); 
-
-            helper(arr, i + 1, currentSum + arr[i], target); 
+            helper(arr, i + 1, currentSum + arr[i], target);
+            helper(arr, i + 1, currentSum - arr[i], target);
         }
-
+        
         return count;
     }
 }
